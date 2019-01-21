7a7b9238a4f84762df059b3ae684be052a36171e
Robert Nowotniak <rnowotniak@gmail.com>
2019-01-21 14:56:50 +0100

    Composer file added, minions support

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM centos:centos7
 MAINTAINER Robert Nowotniak <rnowotniak@gmail.com>
 
 COPY salt.repo /etc/yum.repos.d/salt.repo
-RUN yum -y install salt-master
+RUN yum -y install salt-master salt-minion
 
 COPY run.sh /root/run.sh
 RUN chmod 755 /root/run.sh
 
 
-ENTRYPOINT ["bash", "/root/run.sh" ]
+CMD ["bash", "/root/run.sh" ]
 
--- run.sh
+++ run.sh
@@ -1,1 +1,16 @@
-/usr/bin/salt-master -l info
+
+if [ "$#" -ne 1 -o \( "$1" != master -a "$1" != minion \) ]; then
+	echo "Usage syntax: $0 master|minion"
+	exit 1
+fi
+
+if [ "$1" = master ]; then
+	echo Starting master
+	/usr/bin/salt-master -l info
+else
+	echo Starting minion
+	echo "master: salt1" >> /etc/salt/minion
+	/usr/bin/salt-minion -l info
+fi
+
+
